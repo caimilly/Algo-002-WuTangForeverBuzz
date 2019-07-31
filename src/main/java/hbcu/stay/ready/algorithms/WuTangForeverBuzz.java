26f118923578fa08b6bdb98f761dd26fe73d2f33
cai <crnkl0rd@cais-MacBook-Pro.local>
2019-07-31 04:16:23 -0400

    Simple mistakes always bring me to near death

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,20 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+
+        String number = "";
+        for(int n = 1; n <= 15;n++){
+            if ((n%3==0)&&(n%5==0))
+                number += ("WuTang Forever");
+            //when i had the rest as if's it kept printing out the number and the string instead of replacing
+            // so else if IS important fr
+            else if (n%3==0)
+                number+=("Wu\n");
+            else if (n%5==0)
+                number+=("Tang\n");
+            else
+                number+=n+"\n";
+        }
+        return number;
     }
 }
